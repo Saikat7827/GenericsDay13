39fff8bb122cb550d92cd0106cad52c43f146533
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-10-03 11:56:58 +0530

    Maximum

---
+++ Maximum.java
@@ -0,0 +1,38 @@
+package com.bl.Generics;
+
+
+	public class Maximum<T extends Comparable<T>> {
+		
+		@SafeVarargs
+		public static <T extends Comparable<T>> T testMaximum(T... value) {
+			T max = value[0]; // assume a is initially the largest
+			// checking max value
+			for (int i = 1; i < value.length; i++) {
+				if (value[i].compareTo(max) > 0) {
+					max = value[i];
+				}
+			}
+			System.out.println("given values are : ");
+			for (int i = 0; i < value.length; i++) {
+				System.out.println(value[i]);
+			}
+			printMax(max);// internally calling printMax generic method
+			return max; // returns the largest value
+		}
+
+		// generic method to print the max value
+		public static <T> void printMax(T max) {
+			System.out.println("\nMaximum Value is= " + max + "\n");
+		}
+
+		// main method
+		public static void main(String[] args) {
+			// Calling method And passing values
+			Maximum.<Integer>testMaximum(3, 4, 5, 9, 20, 33);
+
+			Maximum.<Float>testMaximum(6.6f, 8.8f, 7.7f, 9.3f);
+
+			Maximum.<String>testMaximum("Dog", "Cat", "Cow", "Goat", "Buffalo");
+		}
+	}
+
